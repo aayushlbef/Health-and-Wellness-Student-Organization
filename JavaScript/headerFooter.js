9bfefbd6ac3edd2cc9670b2ffb23c6e48b00fdd0
aayushlbef <aayush.gupta@study.lbef.edu.np>
2026-01-16 00:07:41 +0545

    Linking Header and Footer to home page

---
+++ JavaScript/headerFooter.js
@@ -0,0 +1,7 @@
+fetch("../HTML/navigationBar.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("header").innerHTML = data);
+
+fetch("../HTML/footer.html")
+  .then(res => res.text())
+  .then(data => document.getElementById("footer").innerHTML = data);
