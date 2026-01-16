bd5a5ae76e8822f23e310c6632ad37e3c794c5e1
aayushlbef <aayush.gupta@study.lbef.edu.np>
2026-01-16 12:36:28 +0545

    Correcting path

--- JavaScript/headerFooter.js
+++ JavaScript/headerFooter.js
@@ -1,7 +1,7 @@
-fetch("/HTML/navigationBar.html")
+fetch("HTML/navigationBar.html")
   .then(res => res.text())
   .then(data => document.getElementById("header").innerHTML = data);
 
-fetch("/HTML/footer.html")
+fetch("HTML/footer.html")
   .then(res => res.text())
   .then(data => document.getElementById("footer").innerHTML = data);
