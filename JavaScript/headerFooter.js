7c18c7cf370ac8aa9f4932de98a8d78d3d45e564
aayushlbef <aayush.gupta@study.lbef.edu.np>
2026-02-18 22:16:00 +0545

    Updating And fixing small errors

--- JavaScript/headerFooter.js
+++ JavaScript/headerFooter.js
@@ -1,29 +1,7 @@
-// fetch("HTML/navigationBar.html")
-//   .then(res => res.text())
-//   .then(data => document.getElementById("header").innerHTML = data);
-
-// fetch("HTML/footer.html")
-//   .then(res => res.text())
-//   .then(data => document.getElementById("footer").innerHTML = data);
-
-
-// Determine base path dynamically
-const basePath = window.location.pathname.includes("/FDD-Assignment-Team6/")
-  ? "/FDD-Assignment-Team6/"
-  : "/";
-
-// Load header
-fetch(`${basePath}HTML/navigationBar.html`)
+fetch("/HTML/navigationBar.html")
   .then(res => res.text())
-  .then(data => {
-    document.getElementById("header").innerHTML = data;
-  })
-  .catch(err => console.error("Header error:", err));
+  .then(data => document.getElementById("header").innerHTML = data);
 
-// Load footer
-fetch(`${basePath}HTML/footer.html`)
+fetch("/HTML/footer.html")
   .then(res => res.text())
-  .then(data => {
-    document.getElementById("footer").innerHTML = data;
-  })
-  .catch(err => console.error("Footer error:", err));
+  .then(data => document.getElementById("footer").innerHTML = data);
